1185c9c867bf39bc306a154436da9fa6496fc96b
Damiana-Arangio <arangio.damiana@gmail.com>
2025-10-19 23:31:07 +0200

    Milestone 2 completato: - Implementata logica funzioni CRUD (index, show, destroy) - Test eseguito su Postman

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,7 @@
 /************
     IMPORT
 ************/
-const posts = require ('../data/posts.js')       // Import dei dati della risorsa post
+const posts = require ('../data/posts.js')              // Import dei dati della risorsa post
 
 /********************
     FUNZIONI ROTTE
@@ -9,12 +9,14 @@ const posts = require ('../data/posts.js')       // Import dei dati della risors
 
 // index - mostra tutte le ricette
 function index (req, res) {
-    res.send("Lista delle ricette");
+    res.json(posts)
 }
 
 // show - mostra una ricetta specifica
 function show(req, res) {
-    res.send("Dettagli ricetta " + req.params.id);
+    const id = parseInt(req.params.id) ;                  // Recupera stringa "id" dall'URL e la converte in numero
+    const post = posts.find(post => post.id === id);      // Cerca ricetta con l'id specificato nell'array "posts"
+    res.json(post);                                       // Invia ricetta trovata al client
 }
 
 // store - crea una nuova ricetta
@@ -34,7 +36,32 @@ function modify(req, res) {
 
 // destroy - Elimina una ricetta
 function destroy(req, res) {
-    res.send("Eliminata ricetta " + req.params.id)
+    const id = parseInt(req.params.id);                   // Recupera stringa "id" dall'URL e la converte in numero
+    const post = posts.find(post => post.id === id);      // Cerca ricetta con l'id specificato nell'array "posts"
+
+    /* Se la ricetta non esiste: 
+       - Imposta codice di stato HTTP 404 
+       - Invia al client messaggio d'errore
+       - Esci dalla funzione destroy
+    */
+    if(!post)
+        return res.status(404).json (
+            { 
+                status: 404,
+                error: "Not Found" , 
+                message: "Ricetta non trovata"
+            }
+        )
+
+    /* 
+        Se la ricetta esiste:
+        - Elimina post dall'array posts
+        - Invia al client codice di stato http 204 (nessun contenuto)
+          per comunicare l'esito positivo della richiesta
+    */
+   posts.splice(posts.indexOf(post), 1)
+   res.sendStatus(204);
+   console.log(posts); 
 }
 
 
